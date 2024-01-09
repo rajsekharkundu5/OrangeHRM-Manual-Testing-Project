284db20388307a885316b262876045025997297d
rajsekharkundu5 <74252508+rajsekharkundu5@users.noreply.github.com>
2024-01-09 16:45:15 +0530

    Add files via upload

---
+++ ApiGateway/src/test/java/com/hitachi/genai/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hitachi.genai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
